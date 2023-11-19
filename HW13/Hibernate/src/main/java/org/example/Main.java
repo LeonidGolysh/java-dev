566296de1f210bc7741102128c9de0451e3a8560
Leonid <leonidg34@gmail.com>
2023-11-19 14:06:14 +0100

    HW13 Hibernate

---
+++ HW13/Hibernate/src/main/java/org/example/Main.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import org.example.crud.ClientCrudService;
+import org.flywaydb.core.Flyway;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.io.ObjectInputFilter;
+
+public class Main {
+    public static void main(String[] args) {
+        Flyway flyway = Flyway.configure().load();
+        flyway.migrate();
+
+//        Session session = HibernateUtil.getSessionFactory().openSession();
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
+
+        try {
+            transaction = session.beginTransaction();
+//            ClientCrudService clientCrudService = new ClientCrudService((SessionFactory) session);
+            ClientCrudService clientCrudService = new ClientCrudService(sessionFactory);
+            Client newClient = new Client();
+            newClient.setName("Big Bob");
+            clientCrudService.saveClient(newClient);
+
+            Client client = clientCrudService.getClientById(1L);
+            System.out.println("Found client: " + client.getName());
+
+            client.setName("Big Bob");
+            clientCrudService.updateClient(client);
+
+            clientCrudService.deleteClient(client);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+}
